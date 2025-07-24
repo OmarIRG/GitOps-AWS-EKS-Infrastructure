4d95a542e0e4a45aa4a95edc481f646394a50c05
Omar Islam <omarshaalan0100@gmail.com>
2025-07-24 19:32:32 +0300

    Some Modifications

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -1,8 +1,39 @@
-variable "cluster_name"       { description = "EKS cluster name" }
-variable "eks_version"        { description = "Kubernetes version" }
-variable "vpc_id"             { description = "VPC ID" }
-variable "private_subnets"    { description = "Private subnet IDs"  type = list(string) }
-variable "public_subnets"     { description = "Public subnet IDs"   type = list(string) }
-variable "desired_capacity"   { description = "Desired node count" }
-variable "max_capacity"       { description = "Max node count" }
-variable "min_capacity"       { description = "Min node count" }
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+}
+
+variable "eks_version" {
+  description = "Kubernetes version"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "private_subnets" {
+  description = "Private subnet IDs"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "Public subnet IDs"
+  type        = list(string)
+}
+
+variable "desired_capacity" {
+  description = "Desired number of worker nodes"
+  type        = number
+}
+
+variable "max_capacity" {
+  description = "Maximum number of worker nodes"
+  type        = number
+}
+
+variable "min_capacity" {
+  description = "Minimum number of worker nodes"
+  type        = number
+}
--- terraform/modules/rds/variables.tf
+++ terraform/modules/rds/variables.tf
@@ -1,6 +1,31 @@
-variable "db_name"           { description = "Initial DB name" }
-variable "db_username"       { description = "Master username" sensitive = true }
-variable "db_password"       { description = "Master password" sensitive = true }
-variable "vpc_id"            { description = "VPC ID" }
-variable "private_subnets"   { description = "Private subnet IDs"  type = list(string) }
-variable "eks_cluster_sg_id" { description = "EKS cluster SG (for ingress)" }
+variable "db_name" {
+  description = "Initial database name"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Master username"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "Master password"
+  type        = string
+  sensitive   = true
+}
+
+variable "vpc_id" {
+  description = "VPC ID where RDS will live"
+  type        = string
+}
+
+variable "private_subnets" {
+  description = "Private subnet IDs for RDS subnets"
+  type        = list(string)
+}
+
+variable "eks_cluster_sg_id" {
+  description = "Security-group ID of the EKS cluster (for DB ingress)"
+  type        = string
+}
