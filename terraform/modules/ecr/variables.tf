e35e88b3631dcf7553b31fbe3b3381528773e4ba
Omar Islam <omarshaalan0100@gmail.com>
2025-07-24 19:24:11 +0300

    Adding ECR

--- terraform/modules/ecr/variables.tf
+++ terraform/modules/ecr/variables.tf
@@ -1,29 +1,9 @@
-###############################################################################
-# modules/vpc/variables.tf
-###############################################################################
-
-variable "vpc_name" {
+variable "repository_name" {
+  description = "ECR repository name"
   type        = string
-  description = "Name of the VPC"
 }
 
-variable "vpc_cidr" {
+variable "image_tag_mutability" {
+  description = "Tag setting"
   type        = string
-  description = "CIDR block for the VPC"
 }
-
-variable "public_subnets_cidrs" {
-  type        = list(string)
-  description = "List of CIDRs for public subnets"
-}
-
-variable "private_subnets_cidrs" {
-  type        = list(string)
-  description = "List of CIDRs for private subnets"
-}
-
-variable "azs" {
-  type        = list(string)
-  description = "List of availability zones to use"
-}
-
