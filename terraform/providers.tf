009b834e39666958a3efad8c37abe537dc42db83
Omar Islam <omarshaalan0100@gmail.com>
2025-07-24 19:42:35 +0300

    Some Modifications

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,9 +1,15 @@
 terraform {
-  required_version = ">= 1.1.0"
-  backend "local" {}     
+  required_version = ">= 1.3.0"         # ينصح بإصدار حديث من Terraform
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.93.0, < 6.0"      # يلبّي كل قيود الـ Modules
+    }
+  }
+
+  backend "local" {}                    # مازلنا على Backend محلي
 }
 
 provider "aws" {
-  region = "us-east-1"  
+  region = var.aws_region
 }
-
